c2b51d0fe9ac81c51a832d68031e67e4999b4800
EDUARDO VITOR DA SILVA INOCENCIO <eduardo.vsinocencio@senacsp.edu.br>
2026-09-22 22:01:46 -0300

    feat: get variables from API

--- src/components/WheaterApp.jsx
+++ src/components/WheaterApp.jsx
@@ -3,7 +3,28 @@ import { useState } from 'react'
 
 const WheatherApp = () => {
   // GERENCIMENTO E CONTROLE DE DADOS E AÇOES
+  const [data, setData] = useState(null)
   const [location, setLocation] = useState('')
+
+  const getCoordinates = async(cityName) => {
+    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1&language=pt&format=json`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    if (!data.results || data.results.length === 0){
+      throw new Error('Cidade não encontrada')
+    }
+
+    const city = data.results[0]
+
+    return{
+      latitude: city.latitude,
+      longitude: city.longitude,
+      name: city.name,
+      country: city.country
+    }
+  }
 
   const handleInputChanges = (e) => {
     setLocation(e.target.value)
@@ -16,8 +37,37 @@ const WheatherApp = () => {
     }
   }
 
-  const search = (city) => {
-    console.log('Searching for:', city)
+  const search = async (cityName) => {
+    try{
+      // 1. Buscar as coordenadas
+      const coordinates = await getCoordinates(cityName)
+
+      // 2. Pegar a latitude e longitude
+      const { latitude, longitude } = coordinates
+
+      const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code&timezone=auto`.replace(/\s/g,'')
+
+      // 4. Buscar Clima
+      const response = await fetch(url)
+
+      const data = await response.json()
+      
+      console.log('Clima:')
+      console.log(data.current)
+
+      // 5. Salvar os dados obtidos pela API no estado data
+      setData({
+        ...data.current,
+        city: coordinates.name,
+        country: coordinates.country,
+        wheatherType: data.type,
+        whetherDescription: data.description
+      })
+
+
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 
   // ELEMENTOS QUE SERÃO RENDERIZADOS
